4022cb4908a19a7b0d35d302f8ddaef1f2027d25
Maciej <147936508+mmaj1@users.noreply.github.com>
2025-03-20 18:31:14 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,7 @@ module.exports = {
   },
   testMatch: ['**/*.test.ts'],
   moduleDirectories: ['node_modules', '<rootDir>']
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text']
 };
